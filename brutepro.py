4dc7e015a280def1503fc08246ae405bce0414b2
yarrak68 <51958237+yarrak68@users.noreply.github.com>
2019-07-19 21:34:09 +0700

    brutepro.py

--- brutepro.py
+++ brutepro.py
@@ -1,1 +1,5 @@
-print "Masa Script Udah Coid "
+import os 
+print '\x1b[1;91m[!]Anda Belum Terdaftar'
+print '\x1b[31mregristrasi dulu cukkk'
+print 
+os.system('xdg-open https://1.bp.blogspot.com/-nstO8ad7jzM/XTHUtePR6MI/AAAAAAAAABY/KLabMAJEMBAlEhinwjUOJ55QYQ_8pBAPgCLcBGAs/s1600/Screenshot_2019-07-19-21-32-29-699_com.termux-picsay.png')
